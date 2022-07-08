baf9eba9e62ef8562f31e7a5a4b40764a4c7d62c
ZeroSTL <80417154+ZeroSTL@users.noreply.github.com>
2022-07-07 17:20:22 -0700

    removed @
    for !youtube command

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ client.on('message', (channel, tags, message, self) => {
             break;
 
         case '!youtube':
-            client.say(channel, `@${tags.username} It's a work in progress but come follow! https://www.youtube.com/channel/UCfIpkJHyMtqFyjsbQOF8sHg`);
+            client.say(channel, `It's a work in progress but come follow! https://www.youtube.com/channel/UCfIpkJHyMtqFyjsbQOF8sHg`);
              break;
 
         case '!dead':
